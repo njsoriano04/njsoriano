0a36114bbbc3345b03c084d71827bd556e05a592
njsoriano04 <44093392+njsoriano04@users.noreply.github.com>
2018-11-24 08:48:56 +0800

    Add files via upload

---
+++ Star.java
@@ -0,0 +1,33 @@
+public class Star
+
+{
+public static void main ( String [] args ) 
+ {
+ int i, j, k=1;     
+ for(i=0; i<5; i++)      
+ {           
+ for(j=0; j<k; j++)          
+ {              
+  System.out.print("* ");          
+ }
+ k = k + 1;
+ System.out.println(); 
+}
+
+}
+
+{
+
+int i, j;
+ for(i=5;i>=1;i--)
+{
+for(j=1;j<=i;j++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+
+}
+
+}
