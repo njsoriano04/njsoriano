7d6a32659633c1c0512bec98ffd9d45bae47a082
njsoriano04 <44093392+njsoriano04@users.noreply.github.com>
2018-12-01 12:05:28 +0800

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class StudentInfo{
+
+public static void main (String[] args){
+
+
+ Scanner sc = new Scanner(System.in);
+
+
+
+	System.out.println("What is your name?");
+	String name = sc.nextLine();
+        System.out.println("Name:" + " " + name);
+
+	System.out.println("How old are you?");
+	int age = sc.nextInt();
+        System.out.println("Age:"+" " + age);
+
+
+	System.out.println("What is your gender?");
+        String gender = sc.next();
+        System.out.println("Gender:"+" " + gender);
+
+	System.out.println("What is the value of pi?");
+	double pi = sc.nextDouble();
+	System.out.println("Pi:"+" " + pi);
+
+
+
+
+                                      }
+
+                         }
